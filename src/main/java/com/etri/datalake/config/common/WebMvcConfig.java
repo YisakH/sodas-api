58938f6fbcb7b3ee42cc46e3360cb5c0be8060c5
kangwon_dev <dev@kangwon.ac.kr>
2023-09-15 07:00:14 +0000

    feat(cors): add mapping

--- src/main/java/com/etri/datalake/config/common/WebMvcConfig.java
+++ src/main/java/com/etri/datalake/config/common/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.etri.datalake.config.common;
 import com.etri.datalake.auth.GetIdFromTokenArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
         resolvers.add(getIdFromTokenArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                //.allowedOrigins("/**")    //외부에서 들어오는 모둔 url 을 허용
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
+                .allowedHeaders("*")    //허용되는 헤더
+                .allowCredentials(true);    //자격증명 허용
     }
 }
